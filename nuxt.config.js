2e219fa4a13db399e7ecf5d9ccd1cbb680f09079
BayBreezy <53905713+BayBreezy@users.noreply.github.com>
2022-02-14 23:00:54 -0500

    Add fallback to config file for netlify

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,6 +3,10 @@ import colors from "vuetify/es5/util/colors";
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
+  // For netlify
+  generate: {
+    fallback: true
+  },
 
   // Target: https://go.nuxtjs.dev/config-target
   target: "static",
